5d168469d5a4832a88b5ef091edee05e04392188
ryotawattan <ex-j.s.b-0716@outlook.jp>
2021-04-29 21:37:15 +0900

    付加情報用コンポーネントを作成

---
+++ components/Meta.tsx
@@ -0,0 +1,30 @@
+import { IAuthor, ICategory } from "../interfaces/interface"
+
+type MetaProps = {
+    category?: ICategory;
+    author?: IAuthor;
+    createdAt?: string;
+};
+
+export const Meta: React.FC<MetaProps> = (props) => {
+    return (
+        <div>
+            {props.category &&
+                (<span className="category">{props.category.name}</span>)
+            }
+            <div className="meta">
+                <span className="timestamp">
+                    <img src="/images/icon_clock.svg" alt="clock" />
+                </span>
+                {props.author &&
+                    (
+                        <span className="author">
+                            <img src="/images/icon_author.svg" alt="author" />
+                            {props.author.name}
+                        </span>
+                    )
+                }
+            </div>
+        </div>
+    )
+}
