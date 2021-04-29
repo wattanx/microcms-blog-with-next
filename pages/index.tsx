37b08519f056088a0af2335385f88c2b2c75d82d
ryotawattan <ex-j.s.b-0716@outlook.jp>
2021-04-29 16:09:04 +0900

    不要なファイルの削除

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,5 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
-
 const Index = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
+  <h1>Hello Next.js 👋</h1>
 )
 
 export default Index;
