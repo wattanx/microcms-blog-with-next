a160a33a1cbdaf3d5b9689050c4aee2f6da13e78
ryotawattan <ex-j.s.b-0716@outlook.jp>
2021-04-29 15:57:56 +0900

    _app.tsxも追加

---
+++ pages/_app.tsx
@@ -0,0 +1,24 @@
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+import { Header } from '../components/Header';
+import '../styles/globals.scss'
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        
+        <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap"
+          />
+          <link 
+              rel="stylesheet" 
+              href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900&display=swap" 
+            />
+      </Head>
+      <Header />
+      <Component {...pageProps} />
+    </>
+  );
+}
