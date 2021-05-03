844517fec3688d2c843f09a7c611a3dd9d5836b0
ryotawattan <ex-j.s.b-0716@outlook.jp>
2021-05-03 15:56:47 +0900

    SEO対応

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,28 @@ import { AppProps } from 'next/app';
 import Head from 'next/head';
 import { Footer } from '../components/Footer';
 import { Header } from '../components/Header';
+import { config } from '../site.config';
 import '../styles/globals.scss';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const title = config.siteMeta.title;
+  const pageUrl =
+    process.env.NODE_ENV === 'production' ? process.env.BASEURL : 'http://localhost:3000';
+  const description = config.siteMeta.description;
   return (
     <>
       <Head>
+        <title>{title}</title>
+        <meta property="og:title" content={title} />
+        <meta property="og:url" content={pageUrl} />
+        <meta property="og:site" content={config.siteMeta.title} />
+        <meta />
+        {!!description && (
+          <>
+            <meta name="description" content={description} />
+            <meta property="og:description" content={description} />
+          </>
+        )}
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap"
--- site.config.ts
+++ site.config.ts
@@ -1,9 +1,12 @@
 export const config = {
   siteMeta: {
     title: 'microcms-blog-with-next',
-    description: 'microcms-blog-with-next',
+    description: 'microCMS×Next.jsのブログテンプレートです',
   },
-  siteRoot: process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_SITEROOT : 'http://localhost:3000',
+  siteRoot:
+    process.env.NODE_ENV === 'production'
+      ? process.env.NEXT_PUBLIC_SITEROOT
+      : 'http://localhost:3000',
   apiKey: process.env.MICROCMS_APIKEY,
   serviceId: process.env.SERVICE_ID,
   headerLinks: [],
