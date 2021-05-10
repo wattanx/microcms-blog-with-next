ba2d94c3f92c7c502ec714384a8ab0149d6c815d
ryotawattan <ex-j.s.b-0716@outlook.jp>
2021-05-10 22:36:08 +0900

    Tocコンポーネントをステートレスにする

--- components/Toc.tsx
+++ components/Toc.tsx
@@ -1,39 +1,18 @@
 import styles from '../styles/components/Toc.module.scss';
 import { Link } from 'react-scroll';
-import { useEffect, useState } from 'react';
-import { ConvertToHtml, AddList} from '../utils/TocUtil';
-type TocProps = {
-    body: string; 
-}
+import { TocTypes } from '../utils/TocUtil';
 
-type TocItem = {
-    text: string | undefined;
-    id: string | undefined | null;
-    name: string | undefined | null;
+type TocProps = {
+    toc: TocTypes[]; 
+
 }
 
 export const Toc: React.FC<TocProps> = (props) => {
-    const [tocItem, setToc] = useState<TocItem[]>([]);
-    useEffect(() => {
-        const headings: any[] = [];
-        const element = ConvertToHtml(props.body);
-        AddList(headings, element.querySelectorAll('h1, h2, h3, h4, h5, h6'));
-
-        const toc: TocItem[] = headings.map((heading: HTMLHeadElement) => {
-            return {
-                text: heading.innerText,
-                id: heading.getAttribute('id'),
-                name: heading.getAttribute('name')
-            };
-        });
-        setToc(toc);
-    }, [])
-    
     return (
         <div className={styles.wrapper}>
             <h4 className={styles.title}>
                 <ul className={styles.lists}>
-                    {tocItem.map((x) => {
+                    {props.toc.map((x) => {
                         return (
                             <li key={x.id} className={`${styles.list} ${x.name}`}>
                                 <Link to={`${x.id}`}>{x.text}</Link>
